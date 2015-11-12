ca05f10599e2615787af74c02cdc022fbd8845e0
pmbaldha <pmbaldha@gmail.com>
2015-11-12 11:49:24 +0530

    standard coding

--- your-current-location-on-map.php
+++ your-current-location-on-map.php
@@ -21,25 +21,29 @@ You should have received a copy of the GNU General Public License
 along with Your Current Location On Map. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
-add_shortcode( 'your-current-location-on-map','clom_your_current_location_on_map');
-function clom_your_current_location_on_map()
-{ 	
-	wp_enqueue_style(
-		'clom_leaflet_css',
-		plugins_url( '/leaflet/leaflet.css', __FILE__ )
-	);	
-	wp_enqueue_style(
-		'clom_map_css',
-		plugins_url( '/css/map.css', __FILE__ )
-	);	
-	wp_enqueue_script(
-		'clom_leaflet_js',
-		plugins_url( '/leaflet/leaflet-src.js', __FILE__ )
-	);
-	wp_enqueue_script(
-		'clom_map_js',
-		plugins_url( '/js/map-bind.js', __FILE__ ),
-		array('clom_leaflet_js')
-	);
-	echo '<div id="clom_map">map div</div>';
+if ( ! function_exists( 'clom_your_current_location_on_map' ) )
+{
+	function clom_your_current_location_on_map()
+	{ 	
+		wp_enqueue_style(
+			'clom_leaflet_css',
+			plugins_url( '/leaflet/leaflet.css', __FILE__ )
+		);	
+		wp_enqueue_style(
+			'clom_map_css',
+			plugins_url( '/css/map.css', __FILE__ )
+		);	
+		wp_enqueue_script(
+			'clom_leaflet_js',
+			plugins_url( '/leaflet/leaflet-src.js', __FILE__ )
+		);
+		wp_enqueue_script(
+			'clom_map_js',
+			plugins_url( '/js/map-bind.js', __FILE__ ),
+			array('clom_leaflet_js')
+		);
+		echo '<div id="clom_map">map div</div>';
+	}
 }
+if (  function_exists( 'clom_your_current_location_on_map' ) )
+	add_shortcode( 'your-current-location-on-map','clom_your_current_location_on_map');
